80b30e82ba4bface94faff30343e45c7782c96c1
mckennacs <48333899+mckennacs@users.noreply.github.com>
2023-12-06 12:03:54 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
