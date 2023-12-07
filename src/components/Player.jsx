191ac5db7ab0db3641eab3aa0d775f3da45156b1
mckennacs <48333899+mckennacs@users.noreply.github.com>
2023-12-07 21:12:56 +0000

    a lot

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+    // State to determine if user is editing name or not
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    // console.log(isEditing);
+
+    function handleEdit() {
+      // 
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    
+    if (isEditing) {
+        playerName = <input type="text" required value={name}/>;
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
+            <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
